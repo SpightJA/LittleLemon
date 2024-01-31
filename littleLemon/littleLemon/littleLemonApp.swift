334df556db8674e278a914f8748eb2166c3aadc7
Jon Spight <jaspight@hotmail.com>
2024-01-31 17:19:51 -0600

    HomeScreen- Onboarding-Menu view skeletons

--- littleLemon/littleLemon/littleLemonApp.swift
+++ littleLemon/littleLemon/littleLemonApp.swift
@@ -13,7 +13,7 @@ struct littleLemonApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OnboardingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
