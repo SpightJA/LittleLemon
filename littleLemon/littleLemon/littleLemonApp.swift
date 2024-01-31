f9f563aadfdc3678a33bd40d7b521c3e8eddb285
Jon Spight <jaspight@hotmail.com>
2024-01-31 12:31:19 -0600

    intial Commit

---
+++ littleLemon/littleLemon/littleLemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  littleLemonApp.swift
+//  littleLemon
+//
+//  Created by Jon Spight on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct littleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
